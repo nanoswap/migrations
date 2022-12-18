6fbb6b8e69a55902aae10e5aebe129cd77337bf5
Nate Schultz <nate.schultz@outlook.com>
2022-12-17 20:35:47 -0500

    worked on data seed logic

--- src/main.py
+++ src/main.py
@@ -1,14 +1,35 @@
-def seed_wallets():
-    pass
+from typing import List
+import schemas
 
-def seed_loans():
-    pass
-
-def seed_stakes():
-    pass
+# seed users and create wallets for each user
 
 def seed_users():
+    cur_user = None
+
+    wallets = seed_wallets(cur_user)
+
+def seed_wallets(users: schemas.User):
     pass
 
-def seed_payments():
+# seed loans and create stake/payment data for each laon
+
+def seed_loans(users: List[schemas.User], wallets: List[schemas.Wallet]):
+    cur_loan = None
+
+    seed_stakes(cur_loan)
+    seed_payments(cur_loan)
+
+def seed_stakes(loan: schemas.Loan):
     pass
+
+def seed_payments(loan: schemas.Loan):
+    pass
+
+if __name__ == "__main__":
+
+    COUNT_USERS = 100
+    COUNT_LOANS = 50
+    STAKES_PER_LOAN = 10
+
+    users = seed_users(COUNT_USERS)
+    loans = seed_loans(users, COUNT_LOANS, STAKES_PER_LOAN)
