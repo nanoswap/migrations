6a412e65110317b0876c521baa1078ab78df8f36
Nate Schultz <nate.schultz@outlook.com>
2022-12-18 14:09:05 -0500

    worked on seeding wallet status

--- src/main.py
+++ src/main.py
@@ -2,53 +2,46 @@ from typing import List
 import schemas
 import utils
 import random
+import datetime
 
 # seed users and create wallets for each user
 
-def seed_wallet_status():
-    _, unfrozen = utils.insert_wallet_status(schemas.WalletStatus(is_frozen=False, frozen_reason_code=None))
-    _, missing_user_verification = utils.insert_wallet_status(schemas.WalletStatus(is_frozen=True, frozen_reason_code=101))
-    _, fraud_suspected = utils.insert_wallet_status(schemas.WalletStatus(is_frozen=True, frozen_reason_code=102))
-    _, staked_for_loan = utils.insert_wallet_status(schemas.WalletStatus(is_frozen=True, frozen_reason_code=103))
-    return {
-        "unfrozen": unfrozen,
-        "missing_user_verification": missing_user_verification,
-        "fraud_suspected": fraud_suspected,
-        "staked_for_loan": staked_for_loan
-    }
-
-def seed_users(wallet_status: List[schemas.WalletStatus], count_users: int):
+def seed_users(count_users: int):
     for i in range(count_users):
         _, cur_user = utils.insert_user(schemas.User(uid=str(i)))
         utils.insert_wallet(schemas.Wallet(
-            wallet_type=3,
-            address=random.random(),
-            key=random.random(),
-            wallet_status=wallet_status["unfrozen"]
+            owner=cur_user,
+            wallet_type=schemas.WalletType.WITHDRAW_OR_DEPOSIT,
+            address="123",
+            key=None,
+            status=schemas.WalletStatus(
+                state=schemas.WalletState.FROZEN_MISSING_USER_VERIFICATION,
+                is_frozen=True,
+                next=None,
+                previous=None,
+                timestamp=datetime.datetime.date()
+            )
         ))
 
 # seed loans and create stake/payment data for each laon
 
-def seed_loans(users: List[schemas.User], count_loans: int, stakes_per_loan: int):
-    cur_loan = None
+# def seed_loans(users: List[schemas.User], count_loans: int, stakes_per_loan: int):
+#     cur_loan = None
 
-    seed_stakes(cur_loan)
-    seed_payments(cur_loan)
+#     seed_stakes(cur_loan)
+#     seed_payments(cur_loan)
 
-def seed_stakes(loan: schemas.Loan):
-    pass
+# def seed_stakes(loan: schemas.Loan):
+#     pass
 
-def seed_payments(loan: schemas.Loan):
-    pass
+# def seed_payments(loan: schemas.Loan):
+#     pass
 
 if __name__ == "__main__":
 
-    COUNT_USERS = 100
-    COUNT_LOANS = 50
-    STAKES_PER_LOAN = 10
+    COUNT_USERS = 10
+    COUNT_LOANS = 5
+    STAKES_PER_LOAN = 3
 
-    # todo: delete test data if command line arg is provided
-
-    wallet_status = seed_wallet_status()
-    users = seed_users(wallet_status, COUNT_USERS)
-    loans = seed_loans(users, COUNT_LOANS, STAKES_PER_LOAN)
+    users = seed_users(COUNT_USERS)
+    # loans = seed_loans(users, COUNT_LOANS, STAKES_PER_LOAN)
--- src/schemas.py
+++ src/schemas.py
@@ -1,8 +1,23 @@
-from typing import Union, TypeVar, Generic
+from __future__ import annotations
+from typing import Union
 from datetime import datetime
-from pydantic import BaseModel
 from enum import Enum
+from dataclasses import dataclass, asdict, fields
 from random import random
+
+""" Enums """
+
+class WalletType(Enum):
+    WITHDRAW_ONLY = 1, 'yield wallet'
+    DEPOSIT_ONLY = 2, 'bill pay wallet'
+    WITHDRAW_OR_DEPOSIT = 3, 'personal wallet'
+    INTERNAL_ONLY = 4, 'internal wallet'
+
+class WalletState(Enum):
+    FROZEN_MISSING_USER_VERIFICATION: 1
+    FROZEN_FRAUD_SUSPECTED = 2
+    FROZEN_STAKED_FOR_LOAN = 3
+    VALID_ACTIVE_WALLET = 4
 
 class LoanApplicationState(Enum):
     DRAFT = 1
@@ -13,12 +28,6 @@ class LoanApplicationState(Enum):
     DEFAULT = 6
     COMPLETE = 7
 
-class WalletType(Enum):
-    WITHDRAW_ONLY = 1
-    DEPOSIT_ONLY = 2
-    WITHDRAW_OR_DEPOSIT = 3
-    INTERNAL_ONLY = 4
-
 class LoanPaymentState(Enum):
     SCHEDULED = 1
     UPCOMING = 2
@@ -27,168 +36,107 @@ class LoanPaymentState(Enum):
     COMPLETED_LATE = 5
     MISSED = 6
 
-class WalletStatus(object):
+""" Object Status Classes (for tracking entity workflows) """
 
-    reasons = {
-        101: "missing_user_verification",
-        102: "fraud_suspected",
-        103: "staked_for_loan"
-    }
+def post_init(self: object):
+    # convert objects to the dataclass type definition
+    for field in fields(self):
+        value = getattr(self, field.name)
+        if not isinstance(value, field.type):
+            raise setattr(self, field.name, field.type(value))
 
-    def __init__(self, is_frozen, frozen_reason_code):
-        self.doc_id: str = random()
-        self.is_frozen: bool = is_frozen
-        self.frozen_reason_code: int = frozen_reason_code
+@dataclass
+class WalletStatus:
+    state: WalletState
+    is_frozen: bool
+    next: Union[WalletStatus, None]
+    previous: Union[WalletStatus, None]
+    timestamp: datetime
+    doc_id: str = random()
+
+    def __post_init__(self):
+        post_init(self)
+ 
+@dataclass
+class LoanPaymentStatus:
+    state: LoanPaymentState
+    next: Union[LoanPaymentStatus, None]
+    previous: Union[LoanPaymentStatus, None]
+    timestamp: datetime
+    doc_id: str = random()
+
+    def __post_init__(self):
+        post_init(self)
+
+@dataclass   
+class LoanApplicationStatus:
+    state: LoanApplicationState
+    next: Union[LoanApplicationStatus, None]
+    previous: Union[LoanApplicationStatus, None]
+    timestamp: datetime
+    doc_id: str = random()
+
+    def __post_init__(self):
+        post_init(self)
+
+""" Entities """
+
+@dataclass
+class User:
+    uid: str
+
+    def __post_init__(self):
+        post_init(self)
+
+@dataclass   
+class Wallet:
+    owner: Union[User, None]
+    wallet_type: WalletType
+    address: str
+    key: Union[str, None]
+    status: WalletStatus
+    doc_id: float = random()
+
+    def __post_init__(self):
+        post_init(self)
+
+@dataclass
+class Loan:
+    principal_in_xno: float
+    loan_start_date: datetime
+    monthly_payment: float
+    monthly_interest_rate: float
+    number_of_payment_periods: int
+    status: LoanApplicationStatus
+    payment_wallet: Wallet
+    principal_wallet: Wallet
+    borrower: User
+    doc_id: float = random()
+
+    def __post_init__(self):
+        post_init(self)
     
-    def to_dict(self):
-        return {
-            "doc_id": self.doc_id,
-            "is_frozen": self.is_frozen,
-            "frozen_reason_code": self.frozen_reason_code,
-            "frozen_reason_message": self.reasons.get(self.frozen_reason_code)
-        }
+@dataclass
+class Stake:
+    owner: User
+    start_date: datetime
+    from_wallet: Wallet
+    yield_wallet: Wallet
+    monthly_interest_rate: float
+    monthly_payment: float
+    number_of_payment_periods: float
+    doc_id: float = random()
 
-class LoanApplicationStatus(object):
-    def __init__(self, state, next, previous, timestamp):
-        self.doc_id: str = random()
-        self.state: LoanApplicationState = state
-        self.next: Union[LoanApplicationStatus, None] = next
-        self.previous: Union[LoanApplicationStatus, None] = previous
-        self.timestamp: datetime = timestamp
+    def __post_init__(self):
+        post_init(self)
 
-    def to_dict(self):
-        return {
-            "doc_id": self.doc_id,
-            "state": self.LoanPaymentState,
-            "next": self.LoanApplicationStatus,
-            "previous": self.LoanApplicationStatus,
-            "timestamp": self.timestamp
-        }
+@dataclass
+class LoanPayment:
+    loan: Loan
+    due_date: datetime
+    amount_due_in_xno: float
+    status: LoanPaymentStatus
+    doc_id: float = random()
 
-class LoanPaymentStatus(object):
-    def __init__(self, state, next, previous, timestamp):
-        self.doc_id: str = random(),
-        self.state: LoanPaymentState = state
-        self.next: Union[LoanPaymentStatus, None] = next
-        self.previous: Union[LoanPaymentStatus, None] = previous
-        self.timestamp: datetime = timestamp
-        
-    def to_dict(self):
-        return {
-            "doc_id": self.doc_id,
-            "state": self.LoanPaymentState,
-            "next": self.LoanApplicationStatus,
-            "previous": self.LoanApplicationStatus,
-            "timestamp": self.timestamp
-        }
-
-class Wallet(object):
-    def __init__(self, wallet_type, address, key, wallet_status):
-        self.doc_id: str = random()
-        self.wallet_type: WalletType = wallet_type
-        self.address: str = address
-        self.key: str = key
-        self.status: WalletStatus = wallet_status
-        
-    def to_dict(self):
-        # do not return key in responses
-        return {
-            "doc_id": self.doc_id,
-            "wallet_type": self.wallet_type,
-            "address": self.address,
-            "status": self.status
-        }
-
-class User(object):
-    def __init__(self, uid):
-        self.uid: str = uid
-        
-    def to_dict(self):
-        return {
-            "uid": self.uid
-        }
-
-class Loan(object):
-    def __init__(
-            self,
-            principal_in_xno,
-            loan_start_date,
-            monthly_payment,
-            monthly_interest_rate,
-            number_of_payment_periods,
-            status,
-            payment_wallet,
-            principal_wallet,
-            borrower):
-        self.doc_id: str = random()
-        self.principal_in_xno: float = principal_in_xno
-        self.loan_start_date: datetime = loan_start_date
-        self.monthly_payment: float = monthly_payment
-        self.monthly_interest_rate: float = monthly_interest_rate
-        self.number_of_payment_periods: int = number_of_payment_periods
-        self.status: LoanApplicationStatus = status
-        self.payment_wallet: Wallet = payment_wallet
-        self.principal_wallet: Wallet = principal_wallet
-        self.borrower: User = borrower
-        
-    def to_dict(self):
-        return {
-            "doc_id": self.doc_id,
-            "principal_in_xno": self.principal_in_xno,
-            "loan_start_date": self.loan_start_date,
-            "monthly_payment": self.monthly_payment,
-            "monthly_interest_rate": self.monthly_interest_rate,
-            "number_of_payment_periods": self.number_of_payment_periods,
-            "status": self.status,
-            "payment_wallet": self.payment_wallet,
-            "principal_wallet": self.principal_wallet,
-            "borrower": self.borrower
-        }
-
-class Stake(object):
-    def __init__(
-            self,
-            owner,
-            stake_start_date,
-            stake_from_wallet,
-            yield_wallet,
-            monthly_interest_rate,
-            monthly_payment,
-            number_of_payment_periods):
-        self.doc_id: str = random()
-        self.owner: User = owner
-        self.start_date: datetime = stake_start_date
-        self.from_wallet: Wallet = stake_from_wallet
-        self.yield_wallet: Wallet = yield_wallet
-        self.monthly_interest_rate: float = monthly_interest_rate
-        self.monthly_payment: float = monthly_payment
-        self.number_of_payment_periods: float = number_of_payment_periods
-        
-    def to_dict(self):
-        return {
-            "doc_id": self.doc_id,
-            "owner": self.owner,
-            "start_date": self.start_date,
-            "from_wallet": self.from_wallet,
-            "yield_wallet": self.yield_wallet,
-            "monthly_interest_rate": self.monthly_interest_rate,
-            "monthly_payment": self.monthly_payment,
-            "number_of_payment_periods": self.number_of_payment_periods
-        }
-
-class LoanPayment(object):
-    def __init__(self, loan, due_date, amount_due_in_xno, status):
-        self.doc_id: str = random()
-        self.loan: Loan = loan
-        self.due_date: datetime = due_date
-        self.amount_due_in_xno: float = amount_due_in_xno
-        self.status: LoanPaymentStatus = status
-        
-    def to_dict(self):
-        return {
-            "doc_id": self.doc_id,
-            "loan": self.loan,
-            "due_date": self.due_date,
-            "amount_due_in_xno": self.amount_due_in_xno,
-            "status": self.status
-        }
+    def __post_init__(self):
+        post_init(self)
